b96e12681ff2d4e970d936405ea871763fe2490e
Claudwin <claudwin.fortune@gmail.com>
2018-08-10 13:20:41 -0600

    converted request.js to async functions

--- oop/hangman/app.js
+++ oop/hangman/app.js
@@ -1,34 +1,42 @@
 
-const game1 = new Hangman('LeBron James', 3)
 
+
+let game1
 const puzzleEl = document.querySelector('#puzzle')
-puzzleEl.textContent = game1.puzzle
-
 const guessEl = document.querySelector('#guess')
-guessEl.textContent = game1.statusMessage
+
 
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
-    puzzleEl.textContent = game1.puzzle
-    guessEl.textContent = game1.statusMessage
+    render()
     
 })
 
-getPuzzle('3').then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+const render = () => {
+    puzzleEl.textContent = game1.puzzle
+    guessEl.textContent = game1.statusMessage
+}
+const startGame = async () => {
+    const puzzle = await getPuzzle('3')
+    game1 = new Hangman(puzzle, 5)
+    render()
+   }
+   
+   document.querySelector('#reset').addEventListener('click', startGame)
+   
+   startGame()
+   
+// getPuzzle('3').then((puzzle) => {
+//     console.log(puzzle)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
 
-getCountryDetails('NA').then((country) => {
-    console.log(`Country Name: ${country.name}`)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
 
-getLocation().then((data) => {
-    return getCountryDetails(data.country)
-}).then((country) => {
-    console.log(country.name)
-})
+
+// getCurrentCountry().then((country) => {
+//     console.log(country.name) 
+// }).catch((error) => {
+//     console.log(error)
+// })
